cbd4c48161ba1c59c2559701039b978ccdaf8d21
Mayank Solanki <mayanksolanki886@gmail.com>
2024-11-10 15:43:28 +0530

    fabonacci series file added

--- 40tasks/fabonacci.rb
+++ 40tasks/fabonacci.rb
@@ -1,3 +1,14 @@
-input = gets.chomp.to_i #taking input from user
-a = input
-b = 
+#Programm for FABONACCI SERIES
+fab_num = gets.chomp.to_i #taking input from user
+a = 0
+b = 1
+print "#{a} "
+print "#{b} "
+i=2
+while(i<fab_num)
+c = a + b
+print "#{c} "
+a = b
+b = c
+i+=1
+end
